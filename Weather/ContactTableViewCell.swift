add8b00ec0335e3b51972a2d13d4bf4d595e4b59
Brendan Crawford <brendan@wholepunk.com>
2018-04-19 18:55:55 -0700

    Initial project upload

---
+++ Weather/ContactTableViewCell.swift
@@ -0,0 +1,14 @@
+
+import Foundation
+import UIKit
+
+class ContactTableViewCell : UITableViewCell {
+
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: .value2, reuseIdentifier: reuseIdentifier)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}

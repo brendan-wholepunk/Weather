75527196f606a98dbcaec925cddb38b223654b31
Brendan Crawford <brendan@wholepunk.com>
2018-04-19 18:55:48 -0700

    Initial project upload

---
+++ Weather/SettingTableViewCell.swift
@@ -0,0 +1,14 @@
+
+import Foundation
+import UIKit
+
+class SettingTableViewCell : UITableViewCell {
+
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: .value1, reuseIdentifier: reuseIdentifier)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}

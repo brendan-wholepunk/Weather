a3ce6a871f28a6451cf4a6d896fec072b575ef55
Brendan Crawford <brendan@wholepunk.com>
2018-04-19 18:55:49 -0700

    Initial project upload

---
+++ Weather/SubtitleTableViewCell.swift
@@ -0,0 +1,14 @@
+
+import Foundation
+import UIKit
+
+class SubtitleTableViewCell : UITableViewCell {
+
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
